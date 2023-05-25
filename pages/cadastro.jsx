617a77bc97156623789ac5677ad41f4710535178
CamposLeo95 <leocampos.995@gmail.com>
2023-05-25 18:19:22 -0300

    feat(link): put a link in the pages to navegate

--- pages/cadastro.jsx
+++ pages/cadastro.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/pages.module.css';
 
+import Link from 'next/link';
+
 import LoginCard from '../src/components/loginCard/LoginCard';
 import InputLogin from '../src/components/inputLogin/InputLogin';
 import ButtonLogin from '../src/components/buttonLogin/ButtonLogin';
@@ -9,11 +11,12 @@ export default function CadastroPage() {
     <div className={styles.background}>
       <LoginCard title={'Cadastre-se'}>
         <form className={styles.form}>
-          <InputLogin type={'name'} placeholder={'Insira seu nome'} />
-          <InputLogin type={'email'} placeholder={'Insira seu email'} />
-          <InputLogin type={'password'} placeholder={'Insira sua senha'} />
+          <InputLogin type={'name'} placeholder={'Digite seu nome'} />
+          <InputLogin type={'email'} placeholder={'Digite seu email'} />
+          <InputLogin type={'password'} placeholder={'Digite sua senha'} />
           <ButtonLogin>Entrar</ButtonLogin>
         </form>
+        <Link href={'/login'}>Já possui uma conta?</Link>
       </LoginCard>
     </div>
   );
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/pages.module.css';
 
+import Link from 'next/link';
+
 import LoginCard from '../src/components/loginCard/LoginCard';
 import ButtonLogin from '../src/components/buttonLogin/ButtonLogin';
 import InputLogin from '../src/components/inputLogin/InputLogin';
@@ -13,6 +15,7 @@ export default function LoginPage() {
           <InputLogin type={'password'} placeholder={'Digite sua senha'} />
           <ButtonLogin>Entrar</ButtonLogin>
         </form>
+        <Link href={'/cadastro'}>Ainda não possui conta?</Link>
       </LoginCard>
     </div>
   );
