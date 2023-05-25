10a4a389dfda4a7c8b45cb1a16bc1f04db61f75b
CamposLeo95 <leocampos.995@gmail.com>
2023-05-25 16:15:28 -0300

    feat(pages): create pages login and cadastro

---
+++ pages/login.jsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div>
+      <>pagina Login</>
+    </div>
+  );
+}
+export default function Login() {
+    return (
+      <div>
+        <>pagina Login</>
+      </div>
+    );
+  }
