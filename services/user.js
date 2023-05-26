47f403f39581c4ffd6ac6d8134cfcb5e7955b989
CamposLeo95 <leocampos.995@gmail.com>
2023-05-26 10:44:13 -0300

    build(jwt): installing and config the jwt
    I made the jwt configs on the package.json and on the file user.js

--- services/user.js
+++ services/user.js
@@ -1,12 +1,31 @@
+import jwt from 'jsonwebtoken';
+
 let users = [];
+
+const SECRET = process.env.JWT_SECRET;
+
+function createToken(user) {
+  return jwt.sign({ email: user.email, name: user.name }, SECRET);
+}
+
+function readToken(token) {
+  try {
+    //Nesta função ele verifica o token e verifica se foi criado com a chave secreta
+    return jwt.verify(token, SECRET);
+  } catch (error) {
+    throw new Error('Token invalido!');
+  }
+}
 
 export function cadastro(body) {
   const user = users.find(({ email }) => email === body.email);
 
   if (user) throw new Error('Usuário ja cadastrado');
-
   users.push(body);
-  return body;
+
+  const token = createToken(body);
+
+  return token;
 }
 
 export function login(body) {
@@ -15,5 +34,7 @@ export function login(body) {
   if (!user) throw new Error('Usuário não cadastrado');
   if (user.password !== body.password) throw new Error('Usuário ou senha inválidos');
 
-  return user;
+  const token = createToken(user);
+
+  return token;
 }
