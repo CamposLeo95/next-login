89d13bf6f2dc9dd6dc3eb762bf571aa04be23a43
CamposLeo95 <leocampos.995@gmail.com>
2023-05-26 16:06:51 -0300

    feat(form): puting useForm in cadastro page and editing inputlogin to received the hook

--- pages/cadastro.jsx
+++ pages/cadastro.jsx
@@ -1,20 +1,38 @@
+// import { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+
 import styles from '../styles/pages.module.css';
 
 import Link from 'next/link';
 
 import LoginCard from '../src/components/loginCard/LoginCard';
-import InputLogin from '../src/components/inputLogin/InputLogin';
 import ButtonLogin from '../src/components/buttonLogin/ButtonLogin';
+import InputLogin from '../src/components/inputLogin/InputLogin';
 
 export default function CadastroPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  function handleFormSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <div className={styles.background}>
       <LoginCard title={'Cadastre-se'}>
-        <form className={styles.form}>
-          <InputLogin type={'name'} placeholder={'Digite seu nome'} />
-          <InputLogin type={'email'} placeholder={'Digite seu email'} />
-          <InputLogin type={'password'} placeholder={'Digite sua senha'} />
-          <ButtonLogin>Entrar</ButtonLogin>
+        <form className={styles.form} onSubmit={handleSubmit(handleFormSubmit)}>
+          <InputLogin type="name" placeholder="Digite seu nome" register={register('name')} />
+          <InputLogin type="email" placeholder="Digite seu email" register={register('email')} />
+          <InputLogin
+            type="password"
+            placeholder="Digite sua senha"
+            register={register('password')}
+          />
+          <ButtonLogin type="submit">Cadastrar</ButtonLogin>
         </form>
         <Link href={'/login'}>Já possui uma conta?</Link>
       </LoginCard>
--- src/components/inputLogin/InputLogin.jsx
+++ src/components/inputLogin/InputLogin.jsx
@@ -1,5 +1,9 @@
 import styles from './inputLogin.module.css';
 
-export default function InputLogin({ type, placeholder }) {
-  return <input className={styles.input} type={type} placeholder={placeholder} />;
+export default function InputLogin({ register, ...props }) {
+  return (
+    <>
+      <input className={styles.input} {...register} {...props} />
+    </>
+  );
 }
